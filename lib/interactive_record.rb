3375069b25014ca2fa523ebd898296a48ab00c48
Eddie Moon <eddiemoonie@gmail.com>
2018-05-23 19:45:00 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,12 +3,12 @@ require 'active_support/inflector'
 require 'pry'
 
 class InteractiveRecord
-
   def self.table_name
     "#{self.to_s.downcase}s"
   end
 
   def self.column_names
+
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
@@ -25,12 +25,14 @@ class InteractiveRecord
     end
   end
 
-  def table_name_for_insert
-    self.class.table_name
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  def table_name_for_insert
+    self.class.table_name
   end
 
   def values_for_insert
@@ -41,10 +43,8 @@ class InteractiveRecord
     values.join(", ")
   end
 
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def self.find_by_name(name)
@@ -55,9 +55,7 @@ class InteractiveRecord
   def self.find_by(attribute_hash)
     value = attribute_hash.values.first
     formatted_value = value.class == Fixnum ? value : "'#{value}'"
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = '#{formatted_value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     DB[:conn].execute(sql)
-    binding.pry
   end
-
 end
